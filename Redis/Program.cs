36e570799e1229568c5d757de7c0498e811e86f9
Amel Rahmanovic <amelrahmanovic90@gmail.com>
2024-04-09 14:42:33 +0200

    Add project files.

---
+++ Redis/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Redis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisCRUD redisCRUD = new RedisCRUD("localhost");
+
+            int opcija = 1;
+            do
+            {
+                bool succesInput;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("0-Add:");
+                        Console.WriteLine("1-Read");
+                        Console.WriteLine("2-Delete:");
+                        Console.WriteLine("10-EXIT:");
+                        Console.WriteLine("Option:");
+                        opcija = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        succesInput = true;
+                    }
+                    catch (Exception)
+                    {
+                        succesInput = false;
+                        Console.Clear();
+                        Console.WriteLine("This is not number...");
+                    }
+                } while (!succesInput);
+
+
+                switch (opcija)
+                {
+                    case 0:
+                        Console.WriteLine("Added: " + redisCRUD.Save("Test", "test"));
+                        break;
+                    case 1:
+                        var json = redisCRUD.Get("Test");
+                        Console.WriteLine("Result: "+json);
+                        break;
+                    case 2:
+                        Console.WriteLine("Delete: " + redisCRUD.Delete("Test"));
+                        break;
+
+                    case 10: Environment.Exit(0); break;
+                    default: break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Redis/RedisCRUD.cs
@@ -0,0 +1,69 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Redis
+{
+    public class RedisCRUD
+    {
+        private ConnectionMultiplexer _redis;
+        string _connectionString;
+
+        public RedisCRUD(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public bool Save(string Id, string serializedObject)
+        {
+            bool result;
+            try//try to connect from local machine
+            {
+                _redis = ConnectionMultiplexer.Connect(_connectionString+",connectTimeout=10000,responseTimeout=10000");//localhost
+            }
+            catch (Exception)//connect to docker
+            {
+                _redis = ConnectionMultiplexer.Connect("redis:6379,connectTimeout=10000,responseTimeout=10000");//redis is name container
+            }
+
+            var db = _redis.GetDatabase();
+            result = db.StringSet(Id, serializedObject);
+
+            _redis.Close();
+
+            return result;
+        }
+        public string Get(string Id)
+        {
+            string result = "";
+            try//try to connect from local machine
+            {
+                _redis = ConnectionMultiplexer.Connect(_connectionString + ",connectTimeout=10000,responseTimeout=10000");//localhost
+            }
+            catch (Exception)//connect to docker
+            {
+                _redis = ConnectionMultiplexer.Connect("redis:6379,connectTimeout=10000,responseTimeout=10000");//redis is name container
+            }
+
+            var db = _redis.GetDatabase();
+            result = db.StringGet(Id);
+            _redis.Close();
+
+            return result;
+        }
+        public bool Delete(string Id)
+        {
+            try//try to connect from local machine
+            {
+                _redis = ConnectionMultiplexer.Connect(_connectionString + ",connectTimeout=10000,responseTimeout=10000");//localhost
+            }
+            catch (Exception)//connect to docker
+            {
+                _redis = ConnectionMultiplexer.Connect("redis:6379,connectTimeout=10000,responseTimeout=10000");//redis is name container
+            }
+
+            var db = _redis.GetDatabase();
+            bool result = db.KeyDelete(Id);
+            _redis.Close();
+            return result;
+        }
+    }
+}
